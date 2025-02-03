3c031a9fe5498ff8361937dc47e904ddc70daddf
rxciog <rocio.guadalupe.gomez@mi.unc.edu.ar>
2025-02-03 18:17:57 -0300

    hacer Heuristic serializable

--- src/main/java/namedEntities/heuristics/Heuristic.java
+++ src/main/java/namedEntities/heuristics/Heuristic.java
@@ -1,9 +1,10 @@
 package namedEntities.heuristics;
 
+import java.io.Serializable;
 import java.util.List;
 
-public interface Heuristic {
-    
+public interface Heuristic extends Serializable {
+    //Hacer serializable
     public List<String> extractCandidates(String text);
     
 }
