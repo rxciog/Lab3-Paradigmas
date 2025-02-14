f07e770d34a6ab7fd89de2abe3fc5717a2d234ed
rxciog <rocio.guadalupe.gomez@mi.unc.edu.ar>
2025-02-14 18:26:45 -0300

    modificar para usar el nuevo enum de heuristicas

--- src/main/java/namedEntities/heuristics/HeuristicFactory.java
+++ src/main/java/namedEntities/heuristics/HeuristicFactory.java
@@ -6,15 +6,16 @@ import java.util.Set;
 import java.util.function.Supplier;
 
 public class HeuristicFactory {
-    private static Map<String,Supplier<Heuristic>> HEURISTIC_MAP = new HashMap<>();
+    private static Map<HeuristicType,Supplier<Heuristic>> HEURISTIC_MAP = new HashMap<>();
 
     static {
-        HEURISTIC_MAP.put("capital", CapitalizedWordHeuristic::new);
-        HEURISTIC_MAP.put("+capital", CapitaLetterHeuristic::new);
-        HEURISTIC_MAP.put("coreNLP", CoreNLP::new);
+        HEURISTIC_MAP.put(HeuristicType.CAPITAL, CapitalizedWordHeuristic::new);
+        HEURISTIC_MAP.put(HeuristicType.PLUS_CAPITAL, CapitaLetterHeuristic::new);
+        HEURISTIC_MAP.put(HeuristicType.CORE_NLP, CoreNLP::new);
+        HEURISTIC_MAP.put(HeuristicType.PREFIX, PrefixBasedHeuristic::new);
     }
 
-    public static Heuristic createHeuristic(String heuristicName) {
+    public static Heuristic createHeuristic(HeuristicType heuristicName) {
         Supplier<Heuristic> heuristicSupplier = HEURISTIC_MAP.get(heuristicName);
         if (heuristicSupplier == null){
             throw new IllegalArgumentException("Heuristic not found: " + heuristicName);
@@ -23,7 +24,7 @@ public class HeuristicFactory {
         return heuristicSupplier.get();
     }
 
-    public static Set<String> getAvailableHeuristics(){
+    public static Set<HeuristicType> getAvailableHeuristics(){
         return HEURISTIC_MAP.keySet();
     }
 }
