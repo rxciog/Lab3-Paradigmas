7f81c04deff49f5b2b344aca61495daafcaf7392
rxciog <rocio.guadalupe.gomez@mi.unc.edu.ar>
2025-02-14 18:28:18 -0300

    definir enum para formato de estadisticas

---
+++ src/main/java/namedEntities/stats/StatsFormat.java
@@ -0,0 +1,23 @@
+package namedEntities.stats;
+
+public enum StatsFormat {
+    CAT("cat","Category-wise stats"),
+    TOP("top", "Topic-wise stats"),
+    NONE("none","none");
+
+    private final String value;
+    private final String description;
+
+    StatsFormat(String value, String description){
+        this.value = value;
+        this.description = description;
+    }
+
+    public String getValue(){
+        return value;
+    }
+    public String getDescription(){
+        return description;
+    }
+
+}
